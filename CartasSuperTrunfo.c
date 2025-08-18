4f1f34fa082c6bca8e5b7a7ab4e8c5906cc8593f
RochaHub <jean.rocha@live.com>
2025-08-18 18:19:33 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,22 +6,42 @@
 int main() {
     // declarando as variáveis
     
-    char Estado;
-    char Codigo [2];
-    char Cidade [20];
-    int Populacao;
-    float Area;
-    float PIB;
-    int Pontos_turisticos;
+    char estado1; // armazena uma letra representativa de um Estado
+    
+    char codigo1 [2]; // armazena o código da carta
+    
+    char cidade1 [20]; // armazena o nome da cidade
+    
+    int populacao1; // armazena a população da cidade
+    
+    float area1; // armazena a área da cidade em km²
+    
+    float PIB1; // armazena o PIB da cidade
+    
+    int pontos_turisticos1; //armazena a quantidade de pontos turísticos
 
     // interagindo com o usuário e coletando as entradas
     // sintaxe do scanf "%formato1 %formato2 %formato3", &variavel1, &variavel2, &variavel3
 
-    printf ("Digite uma letra representativa do Estado. Varia de 'A' a 'H'");
-    scanf ("%c", &Estado);
+    printf ("Digite uma letra representativa do Estado. Varia de 'A' a 'H': ");
+    scanf ("%c", &estado);
 
-    printf ("Digite o Código da Carta com 2 algarismos numéricos");
-    scanf ("%s", &Codigo);
+    printf ("Digite o Código da Carta com 2 algarismos numéricos: ");
+    scanf ("%s", &codigo);
+
+    printf ("Digite o nome da Cidade: ");
+    scanf ("%s", &cidade);
+
+    printf ("Digite o número de habitantes da cidade: ");
+    scanf ("%d", &populacao);
+
+    printf ("Digite a área da cidade em km²: );
+    scanf ("%f", &area);
+
+    
+
+    
+        
     
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
