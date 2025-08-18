e314bbbf156f74360c549245794344f4dd12ace6
RochaHub <jean.rocha@live.com>
2025-08-18 17:51:31 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países - by Jean Rocha
+// Desafio Super Trunfo - Países - Jean Rocha
 // Tema 1 - Cadastro das Cartas
 
 int main() {
-    char Estado [20];
-    char codigo [3];
+    // declarando as variáveis
     
+    char Estado;
+    char Codigo [2];
+    char Cidade [20];
+    int Populacao;
+    float Area;
+    float PIB;
+    int Pontos_turisticos;
+
+    // interagindo com o usuário e coletando as entradas
+    // sintaxe do scanf "%formato1 %formato2 %formato3", &variavel1, &variavel2, &variavel3
+
+    printf ("Digite uma letra representativa do Estado. Varia de 'A' a 'H'");
+    scanf ("%c", &Estado);
+
+    printf ("Digite o Código da Carta com 2 algarismos numéricos");
+    scanf ("%s", &Codigo);
     
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
