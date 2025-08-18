ce9bd69b36644539530b5448e1efe7f3d36164c9
RochaHub <jean.rocha@live.com>
2025-08-18 08:51:14 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+// Desafio Super Trunfo - Países - by Jean Rocha
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
+    int Estado;
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
